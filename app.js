0b0ca7bc3cd19d85436173ac33dbede6417bab0e
luisve9 <luissamdoval.9@gmail.com>
2021-06-26 12:25:27 -0500

    Initial configuration Back

---
+++ app.js
@@ -0,0 +1,63 @@
+require("dotenv").config()
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+mongoose
+    .connect( process.env.DB , {
+        useCreateIndex:true,
+        useUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
+
+const app = express();
+
+app.use(
+    cors({
+        origins:["http://localhost:3000"/*, "Tambien darle permiso a aplicacion deployada"*/], 
+    })
+)
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
